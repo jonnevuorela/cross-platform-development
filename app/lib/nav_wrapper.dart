4c1b2ef7334414daebebe6724399c66f72649c14
jonne <jonne.vuorela@proton.me>
2026-04-07 15:17:59 +0300

    nav wrapper

---
+++ app/lib/nav_wrapper.dart
@@ -0,0 +1,68 @@
+import 'package:common_github_search/common_github_search.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'counter_bloc.dart';
+import 'counter_page.dart';
+import 'drawing.dart';
+import 'search_form.dart';
+
+class NavWrapper extends StatefulWidget {
+  const NavWrapper({super.key});
+
+  @override
+  State<NavWrapper> createState() => _NavWrapperState();
+}
+
+class _NavWrapperState extends State<NavWrapper> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (_) => GithubRepository(),
+      dispose: (repository) => repository.dispose(),
+      child: MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
+        ),
+        home: Scaffold(
+          body: IndexedStack(
+            index: _currentIndex,
+            children: [
+              BlocProvider(create: (_) => CounterBloc(), child: CounterPage()),
+              BlocProvider(
+                create: (context) => GithubSearchBloc(
+                  githubRepository: context.read<GithubRepository>(),
+                ),
+                child: Scaffold(
+                  appBar: AppBar(title: Text('GitHub Search')),
+                  body: SearchForm(),
+                ),
+              ),
+              const DrawingPage(title: 'Drawing'),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: (index) => setState(() => _currentIndex = index),
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add_circle),
+                label: 'Counter',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Search',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.brush),
+                label: 'Drawing',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
