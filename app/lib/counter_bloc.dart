f0c8466a5c61fdd351234cd3f3f4f80ba7946f20
jonne <jonne.vuorela@proton.me>
2026-04-07 15:17:19 +0300

    bloc counter example

---
+++ app/lib/counter_bloc.dart
@@ -0,0 +1,38 @@
+import 'package:app/main.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+sealed class CounterEvent {}
+
+final class CounterIncrementPressed extends CounterEvent {}
+final class CounterDecrementPressed extends CounterEvent {}
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0) {
+    on<CounterIncrementPressed>((event, emit) => emit(state + 1));
+    on<CounterDecrementPressed>((event, emit) => emit(state - 1));
+  }
+
+  @override
+  void onEvent(CounterEvent event) {
+    super.onEvent(event);
+    print(event);
+  }
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+  }
+
+  @override
+  void onTransition(Transition<CounterEvent, int> transition) {
+    super.onTransition(transition);
+    print(transition);
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    print('$error, $stackTrace');
+    super.onError(error, stackTrace);
+  }
+}
