b60a5e013311363e0edf8f245ab7742905d61766
jonne <jonne.vuorela@proton.me>
2026-05-08 10:33:15 +0300

    chopped tests to smaller ones

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:app/counter_bloc.dart';
 import 'package:app/counter_page.dart';
 import 'package:flutter/material.dart';
@@ -5,59 +7,70 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      BlocProvider(
-        create: (_) => CounterBloc(),
-        child: MaterialApp(home: CounterPage()),
-      ),
+  group('CounterPage', () {
+    var counter = BlocProvider(
+      create: (_) => CounterBloc(),
+      child: MaterialApp(home: CounterPage()),
     );
 
-    // Increment all the way to 3 and decrease back to zero but not negative.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    testWidgets('Counter smoke test', (WidgetTester tester) async {
+      await tester.pumpWidget(counter);
 
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
+    });
 
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    testWidgets('Counter increment test', (WidgetTester tester) async {
+      await tester.pumpWidget(counter);
 
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
 
-    expect(find.text('1'), findsNothing);
-    expect(find.text('2'), findsOneWidget);
-    
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
 
-    expect(find.text('2'), findsNothing);
-    expect(find.text('3'), findsOneWidget);
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
 
-    await tester.tap(find.byIcon(Icons.remove));
-    await tester.pump();
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsNothing);
+      expect(find.text('2'), findsOneWidget);
+    });
 
-    expect(find.text('3'), findsNothing);
-    expect(find.text('2'), findsOneWidget);
+    testWidgets('Counter decrement test', (WidgetTester tester) async {
+      await tester.pumpWidget(counter);
 
-    await tester.tap(find.byIcon(Icons.remove));
-    await tester.pump();
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
 
-    expect(find.text('2'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
 
-    await tester.tap(find.byIcon(Icons.remove));
-    await tester.pump();
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
 
-    expect(find.text('1'), findsNothing);
-    expect(find.text('0'), findsOneWidget);
-    
-    await tester.tap(find.byIcon(Icons.remove));
-    await tester.pump();
+      await tester.tap(find.byIcon(Icons.remove));
+      await tester.pump();
 
-    expect(find.text('1'), findsNothing);
-    expect(find.text('-1'), findsNothing);
-    expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
+      expect(find.text('0'), findsOneWidget);
+    });
+
+    testWidgets('Counter decrement constraint test', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(counter);
+
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
+
+      await tester.tap(find.byIcon(Icons.remove));
+      await tester.pump();
+
+      expect(find.text('1'), findsNothing);
+      expect(find.text('-1'), findsNothing);
+      expect(find.text('0'), findsOneWidget);
+    });
+
   });
 }
