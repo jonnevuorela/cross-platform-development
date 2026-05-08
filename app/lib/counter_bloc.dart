bc428937b0bb1c4a2936a6ae546027b8bfd52b4c
jonne <jonne.vuorela@proton.me>
2026-05-08 09:49:40 +0300

    added constrain feature for testability

--- app/lib/counter_bloc.dart
+++ app/lib/counter_bloc.dart
@@ -4,12 +4,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 sealed class CounterEvent {}
 
 final class CounterIncrementPressed extends CounterEvent {}
+
 final class CounterDecrementPressed extends CounterEvent {}
 
 class CounterBloc extends Bloc<CounterEvent, int> {
   CounterBloc() : super(0) {
     on<CounterIncrementPressed>((event, emit) => emit(state + 1));
-    on<CounterDecrementPressed>((event, emit) => emit(state - 1));
+    on<CounterDecrementPressed>((event, emit) {
+      if (state > 0) {
+        emit(state - 1);
+      } else {
+        emit(0);
+      }
+    });
   }
 
   @override
