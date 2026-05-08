5da555bd27036c0b9a0be842368217996bfeabe4
jonne <jonne.vuorela@proton.me>
2026-05-08 09:49:57 +0300

    expanded test

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -1,30 +1,63 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:app/counter_bloc.dart';
+import 'package:app/counter_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:app/nav_wrapper.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const NavWrapper());
+    await tester.pumpWidget(
+      BlocProvider(
+        create: (_) => CounterBloc(),
+        child: MaterialApp(home: CounterPage()),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
+    // Increment all the way to 3 and decrease back to zero but not negative.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    expect(find.text('1'), findsNothing);
+    expect(find.text('2'), findsOneWidget);
+    
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    expect(find.text('2'), findsNothing);
+    expect(find.text('3'), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.pump();
+
+    expect(find.text('3'), findsNothing);
+    expect(find.text('2'), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.pump();
+
+    expect(find.text('2'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.pump();
+
+    expect(find.text('1'), findsNothing);
+    expect(find.text('0'), findsOneWidget);
+    
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.pump();
+
+    expect(find.text('1'), findsNothing);
+    expect(find.text('-1'), findsNothing);
+    expect(find.text('0'), findsOneWidget);
   });
 }
