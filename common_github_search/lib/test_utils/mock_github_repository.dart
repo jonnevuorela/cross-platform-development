31d26272bc2149de18408d93f9550412551e942e
jonne <jonne.vuorela@proton.me>
2026-05-09 14:39:52 +0300

    overload to create mock data for the repository

--- common_github_search/lib/test_utils/mock_github_repository.dart
+++ common_github_search/lib/test_utils/mock_github_repository.dart
@@ -1,4 +1,18 @@
 import 'package:common_github_search/common_github_search.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockGithubRepository extends Mock implements GithubRepository {}
+class MockGithubRepository extends Mock implements GithubRepository {
+  // Overload to provide testing data with repository.
+  MockGithubRepository({bool withMockData = false}) {
+    if (withMockData) {
+      when(
+        () => search('flutter'),
+      ).thenAnswer((_) async => createMockSearchResult(itemCount: 8));
+
+      when(
+        () => search(any()),
+      ).thenAnswer((_) async => createMockSearchResult(itemCount: 3));
+    }
+  }
+}
+
